8eb3d96b96d7b2c228f894f60d7299c26971fc05
musubi42 <raphael.dev@protonmail.com>
2022-09-29 10:02:55 +0200

    Init new archi

---
+++ Back/api/models/index.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize('mysql://admin:admin@localhost:3307/tutorial') // Example for postgres
+
+// Option 2: Passing parameters separately (sqlite)
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// });
+
+// Option 3: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+// });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
---
+++ Back/api/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8091"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// Raccord Sequelize
+const db = require("./models");
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Successfully connected to the database.");
+})
+.catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// Our routes
+require("./routes/tutorials.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8090;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
