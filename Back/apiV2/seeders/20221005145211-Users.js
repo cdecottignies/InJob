3dd44d7bec5d7fab38f346215778bae35c61a051
musubi42 <raphael.dev@protonmail.com>
2022-10-05 18:40:29 +0200

    Init User Seeders

---
+++ Back/apiV2/seeders/20221005145211-Users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.bulkInsert('Users', [{
+      firstName: 'Jon',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      password: 'p@ssword',
+      phone: 0102030405,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Dae',
+      email: 'jane.dae@example.com',
+      password: 'p@ssword',
+      phone: 0102030405,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      firstName: 'Nick',
+      lastName: 'Mark',
+      email: 'nick.mark@example.com',
+      password: 'p@ssword',
+      phone: 0102030405,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.bulkDelete('Users', null, {});
+  }
+};
