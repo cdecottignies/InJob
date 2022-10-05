ebff2f54d3701c934cb98b20c1ad864b30934d14
musubi42 <raphael.dev@protonmail.com>
2022-10-05 18:38:10 +0200

    Sequelize Init advertisement Model & Migration

---
+++ Back/apiV2/migrations/20221005143440-create-advertisements.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Advertisements', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      descShort: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      descLong: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      wages: {
+        type: Sequelize.BIGINT,
+        allowNull: false
+      },
+      place: {
+        type: Sequelize.JSON,
+        allowNull: false
+      },
+      workingTime: {
+        type: Sequelize.ENUM('flexible', '9 to 5'),
+        allowNull: false
+      },
+      workingLocation: {
+        type: Sequelize.ENUM('remote', 'office', 'hybrid'),
+        allowNull: false
+      },
+      hybrid: {
+        type: Sequelize.ENUM('0', '1 day remote, 4 days office', '2 days remote, 3 days office', '3 days remote, 2 days office', '4 days remote, 1 day office'),
+        defaultValue: '0',
+        allowNull: false
+      },
+      contractType: {
+        type: Sequelize.ENUM('CDI', 'CDD', 'ALTERNANCE'),
+        allowNull: false
+      },
+      contractLength: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      contractStart: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Advertisements');
+  }
+};
---
+++ Back/apiV2/models/advertisements.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Advertisements extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Advertisements.hasMany(models.User, {
+        foreignKey: 'userId',
+      });
+    }
+  }
+  Advertisements.init({
+    title: DataTypes.STRING,
+    descShort: DataTypes.STRING,
+    descLong: DataTypes.TEXT,
+    wages: DataTypes.BIGINT,
+    place: DataTypes.JSON,
+    degree: DataTypes.ENUM('BAC+1', 'BAC+2', 'BAC+3', 'BAC+4', 'BAC+5'),
+    workingTime: DataTypes.ENUM('flexible', '9 to 5'),
+    workLocation: DataTypes.ENUM('remote', 'office', 'hybrid'),
+    hybrid: DataTypes.ENUM('0', '1 day remote, 4 days office', '2 days remote, 3 days office', '3 days remote, 2 days office', '4 days remote, 1 day office'),
+    contractType: DataTypes.ENUM('CDI', 'CDD', 'ALTERNANCE'),
+    contractLength: DataTypes.STRING,
+    contractStart: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Advertisements',
+  });
+  return Advertisements;
+};
