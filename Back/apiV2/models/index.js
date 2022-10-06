2debf78758072b9a3b6b3499d9b2148a15e51a67
musubi42 <raphael.dev@protonmail.com>
2022-10-06 14:43:46 +0200

    Need to link model to sequelize

--- Back/apiV2/models/index.js
+++ Back/apiV2/models/index.js
@@ -26,6 +26,7 @@ fs
     db[model.name] = model;
   });
 
+// Ca ne fonctionne pas des masses
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
@@ -34,5 +35,9 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+// Ca fonctionnel
+db.advertisements = require("./advertisements.js")(sequelize, Sequelize);
+
 
 module.exports = db;
