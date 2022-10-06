5b38a07d31d84482cfb4926cb31e82ea290fa569
musubi42 <raphael.dev@protonmail.com>
2022-10-06 14:51:48 +0200

    Import/Export all routes with the same file

---
+++ Back/apiV2/routes/index.js
@@ -0,0 +1,9 @@
+const advertisements = require('./advertisements.route');
+const companies = require('./companies.route');
+const users = require('./users.route');
+
+module.exports = {
+    advertisements,
+    companies,
+    users
+}
