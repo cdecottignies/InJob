f6343180e9b587084292b7936c523e374eb782d6
musubi42 <raphael.dev@protonmail.com>
2022-10-06 14:51:31 +0200

    Init users route

---
+++ Back/apiV2/routes/users.route.js
@@ -0,0 +1,26 @@
+const users = require("../controllers/users.controller.js");
+
+var router = require("express").Router();
+
+// Create a new Advertisements
+router.post("/", users.create);
+
+// Retrieve all Advertisements
+router.get("/", users.findAll);
+
+// Retrieve all published Advertisements
+router.get("/published", users.findAllPublished);
+
+// Retrieve a single Advertisements with id
+router.get("/:id", users.findOne);
+
+// Update a Advertisements with id
+router.put("/:id", users.update);
+
+// Delete a Advertisements with id
+router.delete("/:id", users.delete);
+
+// Delete all Advertisements
+router.delete("/", users.deleteAll);
+  
+module.exports = router;
