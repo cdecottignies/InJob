6ca0abcc95c1cb40afdd8f0a0185dc9b5f96b0b1
musubi42 <raphael.dev@protonmail.com>
2022-10-06 14:44:04 +0200

    Ad clean route

---
+++ Back/apiV2/routes/advertisements.route.js
@@ -0,0 +1,28 @@
+const advertisementsController = require("../controllers/advertisements.controller.js");
+
+var router = require("express").Router();
+
+// Create a new Advertisements
+router.post("/", advertisementsController.create);
+
+// Retrieve all Advertisements
+router.get("/", advertisementsController.findAll);
+
+// Retrieve all published Advertisements
+router.get("/published", advertisementsController.findAllPublished);
+
+// Retrieve a single Advertisements with id
+router.get("/:id", advertisementsController.findOne);
+
+// Update a Advertisements with id
+router.put("/:id", advertisementsController.update);
+
+// Delete a Advertisements with id
+router.delete("/:id", advertisementsController.delete);
+
+// Delete all Advertisements
+router.delete("/", advertisementsController.deleteAll);
+
+// app.use('/api/advertisements', router);
+
+module.exports = router;
