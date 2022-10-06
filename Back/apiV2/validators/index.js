388fe18a6b847221e265377b8ab3e2f386464e06
musubi42 <raphael.dev@protonmail.com>
2022-10-06 16:12:54 +0200

    Export all Joi Schema

---
+++ Back/apiV2/validators/index.js
@@ -0,0 +1,9 @@
+const advertisements = require('./advertisements.validator')
+const companies = require('./companies.validator')
+const users = require('./users.validator')
+
+module.exports = {
+    advertisements,
+    companies,
+    users
+}
