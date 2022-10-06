0767b1fa0c039051ccda60b968026d86e3b5a1fc
musubi42 <raphael.dev@protonmail.com>
2022-10-06 15:42:21 +0200

    Allow the Front

--- Back/apiV2/server.js
+++ Back/apiV2/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
 const Joi = require('joi');
+const cors = require("cors");
+
 
 const PORT = process.env.PORT || 8090;
 
 // Add midleware
 const app = express();
+
+// Parse Json
 app.use(express.json());
+
+// Allow the front to communicate with the Back
+var corsOptions = {
+    origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
 
 // Import API routes
 app.use('/api/advertisements', routes.advertisements);
