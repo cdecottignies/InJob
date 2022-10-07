269c5542048870aa1735cbad77f7145b6345e198
musubi42 <raphael.dev@protonmail.com>
2022-10-07 16:26:24 +0200

    export auth route

--- Back/apiV2/routes/index.js
+++ Back/apiV2/routes/index.js
@@ -1,9 +1,11 @@
 const advertisements = require('./advertisements.route');
 const companies = require('./companies.route');
 const users = require('./users.route');
+const auth = require('./auth.route');
 
 module.exports = {
     advertisements,
     companies,
-    users
+    users,
+    auth
 }
