9bd656ea107114a4294c9eb801a6e9bf9b353bd6
musubi42 <raphael.dev@protonmail.com>
2022-10-11 09:44:41 +0200

    Delete data we dont use anymore

--- Back/apiV2/tests/advertisements.test.js
+++ Back/apiV2/tests/advertisements.test.js
@@ -2,9 +2,52 @@ const request = require('supertest')
 const app = require('../server')
 
 describe('Advertisements', () => {
-    it('should show all advertisements', async () => {
+    it('POST /advertisements --> create a new ad', async () => {
+        const res = await request(app)
+        .post('/api/advertisements')
+        .send({
+            title: "je",
+            descShort: "Je suis",
+            descLong: "Je suis toi",
+            wages: 1900,
+            place: { "city": "Lille", "street": "Liberte"},
+            degree: "BAC+1",
+            workingTime: "flexible",
+            workingLocation: "remote",
+            contractType: "CDI",
+            contractStart: 1665071052
+        })
+        expect(res.status).toEqual(201)
+        expect(res.body.title).toBe("je")
+        expect(res.body.descShort).toBe("Je suis")
+        expect(res.body.descLong).toBe("Je suis toi")
+        expect(res.body.wages).toBe(1900)
+        expect(res.body.place).toEqual({ "city": "Lille", "street": "Liberte"})
+        expect(res.body.degree).toBe("BAC+1")
+        expect(res.body.workingTime).toBe("flexible")
+        expect(res.body.workingLocation).toBe("remote")
+        expect(res.body.contractType).toBe("CDI")
+        expect(res.body.contractStart).toEqual("1970-01-20")
+    });
+    
+    it('GET /advertisements --> specific ad by ID', async () => {
+        return request(app)
+        .get('/api/advertisements/1')
+        .expect(res.status).toEqual(200)
+        .expect('Content-Type', /json/)
+        
+    });
+
+    it('GET /advertisements --> get all ads', async () => {
         const res = await request(app).get('/api/advertisements')
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveProperty('title')
-    })
+        expect(res.status).toEqual(200)
+    });
+
+    // it('should show all advertisements', async () => {
+    //     const res = await request(app).get('/api/advertisements')
+    //     expect(res.statusCode).toEqual(200)
+    //     expect(res.body).to
+    // });
+
+    
 })
--- Back/apiV2/validators/advertisements.validator.js
+++ Back/apiV2/validators/advertisements.validator.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
   
+// hybrid: Joi.string().valid('0', '1 day remote, 4 days office', '2 days remote, 3 days office', '3 days remote, 2 days office', '4 days remote, 1 day office').required(),
+// contractLength: Joi.string(),
 exports.createSchema = Joi.object().keys({
     title: Joi.string().required(),
     descShort: Joi.string().required(),
@@ -12,9 +14,7 @@ exports.createSchema = Joi.object().keys({
     degree: Joi.string().valid('BAC+1', 'BAC+2', 'BAC+3', 'BAC+4', 'BAC+5').required(),
     workingTime: Joi.string().valid('flexible', '9 to 5').required(),
     workingLocation: Joi.string().valid('remote', 'office', 'hybrid').required(),
-    hybrid: Joi.string().valid('0', '1 day remote, 4 days office', '2 days remote, 3 days office', '3 days remote, 2 days office', '4 days remote, 1 day office').required(),
     contractType: Joi.string().valid('CDI', 'CDD', 'ALTERNANCE').required(),
-    contractLength: Joi.string(),
     contractStart: Joi.date().timestamp(),
 });
 
@@ -22,6 +22,8 @@ exports.findOneSchema = Joi.object().keys({
     id: Joi.string().required(),
 });
 
+// hybrid: Joi.string().valid('0', '1 day remote, 4 days office', '2 days remote, 3 days office', '3 days remote, 2 days office', '4 days remote, 1 day office').required(),
+// contractLength: Joi.string(),
 exports.updateSchema = Joi.object().keys({
     title: Joi.string().required(),
     descShort: Joi.string().required(),
@@ -34,9 +36,7 @@ exports.updateSchema = Joi.object().keys({
     degree: Joi.string().valid('BAC+1', 'BAC+2', 'BAC+3', 'BAC+4', 'BAC+5').required(),
     workingTime: Joi.string().valid('flexible', '9 to 5').required(),
     workingLocation: Joi.string().valid('remote', 'office', 'hybrid').required(),
-    hybrid: Joi.string().valid('0', '1 day remote, 4 days office', '2 days remote, 3 days office', '3 days remote, 2 days office', '4 days remote, 1 day office').required(),
     contractType: Joi.string().valid('CDI', 'CDD', 'ALTERNANCE').required(),
-    contractLength: Joi.string(),
     contractStart: Joi.date().timestamp(),
 });
 
